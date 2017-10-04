09893ebe359ba875aacf1d76390d6f47787831b3
Inzamam Malik <malikasinger1@gmail.com>
2017-10-04 10:52:11 +0500

    get user done

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ admin.initializeApp(functions.config().firebase);
 
 var db = admin.firestore();
 
+// create a user in db: provide all required (id, firstName, lastName, dob) in request body when calling this http trigger
 exports.creteUser = functions.https.onRequest((req, res) => {
 
     var docRef = db.collection('users').doc(req.body.id);
@@ -22,17 +23,80 @@ exports.creteUser = functions.https.onRequest((req, res) => {
             console.log("user added with id: " + ref.id);
             res.send("user added with id: " + ref.id)
         });
-});
 
-exports.getUser = functions.https.onRequest((req, res) => {
+
+    // AUTO GENRATED PUSH ID APPROACH
+
+    // 1)
+    // var docRef = db.collection('users');
+    // docRef.add({
+    //     firstName: req.body.firstName,
+    //     lastName: req.body.lastName,
+    //     dob: req.body.dob
+    // })
+
+    // 2)
+    // var docRef = db.collection('users').doc();
+    // docRef.set({
+    //     firstName: req.body.firstName,
+    //     lastName: req.body.lastName,
+    //     dob: req.body.dob
+    // })
+});
+// get whole collection of users in json at once
+exports.getUsers = functions.https.onRequest((req, res) => {
 
     db.collection('users').get()
-        .then((snapshot) => {
-            snapshot.forEach((doc) => {
-                console.log(doc.id, '=>', doc.data());
-            });
-        })
+
         .catch((err) => {
             console.log('Error getting documents', err);
+            res.send('Error getting documents, check logs')
+        })
+
+        .then((docSnapshot) => {
+            // snapshot.forEach((doc) => {
+            //     console.log(doc.id, '=>', doc.data());
+            // });
+            if (!docSnapshot.exists) {
+                console.log('No such document!');
+                res.send('No such document!');
+            } else {
+                console.log('Document data:', docSnapshot.data());
+                res.send(docSnapshot.data())
+            }
         });
+});
+
+// get single user, provide user id in request body and it will return user
+exports.getUser = functions.https.onRequest((req, res) => {
+
+    db.collection('users').doc(req.body.id).get()
+        .catch((err) => {
+            console.log('Error getting documents', err);
+            res.send('Error getting documents, check logs')
+        })
+
+        .then((docSnapshot) => {
+
+            if (!docSnapshot.exists) {
+                console.log('No such document!');
+                res.send('No such document!');
+            } else {
+                console.log('Document data:', docSnapshot.data());
+                res.send(docSnapshot.data())
+            }
+        });
+
+    // OTHER APPROACHES OF SAME THING:
+    // db.collection('users').where('id','==', req.body.id)
+    //     .then((docSnapshot) => {
+
+    //         if (!docSnapshot.exists) {
+    //             console.log('No such document!');
+    //             res.send('No such document!');
+    //         } else {
+    //             console.log('Document data:', docSnapshot.data());
+    //             res.send(docSnapshot.data())
+    //         }
+    //     });
 });
